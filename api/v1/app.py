e1d7642553c12504621f27d881d5a97e90090704
“Achraf <achrafassouam@gmail.com>
2024-05-26 18:00:27 +0100

    adding 404 error page in app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -16,6 +16,14 @@ def teardown_appcontext(code):
     storage.close()
 
 
+@app.errorhandler(404)
+def not_found(error):
+    """
+    Returns a JSON-formatted 404 status code response
+    """
+    return jsonify({"error": "Not found"}), 404
+
+
 if __name__ == "__main__":
     app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
             port=int(os.getenv('HBNB_API_PORT', '5000')))
