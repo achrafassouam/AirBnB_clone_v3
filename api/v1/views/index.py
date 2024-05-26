2b9cd908db8b272f8da45dee26ff766b59f91e34
“Achraf <achrafassouam@gmail.com>
2024-05-26 17:40:13 +0100

    updating pycodestyle for files in /api

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,10 +4,12 @@ from api.v1.views import app_views
 from flask import Flask, Blueprint, jsonify
 from models import storage
 
+
 @app_views.route('/status', strict_slashes=False)
 def hbnbStatus():
     """hbnbStatus"""
     return jsonify({"status": "OK"})
+
 
 if __name__ == "__main__":
     pass
