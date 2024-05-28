212a4e23fccca9946c9068a81d4d41e29b96df24
“Achraf <achrafassouam@gmail.com>
2024-05-28 22:11:28 +0100

    Updating states.py

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -17,7 +17,8 @@ def get_states():
     return jsonify(states)
 
 
-@app_views.route('/states/<string:state_id>', methods=['GET'], strict_slashes=False)
+@app_views.route('/states/<string:state_id>', methods=['GET'],
+                 strict_slashes=False)
 def get_state(state_id):
     """get state information for specified state"""
     state = storage.get("State", state_id)
