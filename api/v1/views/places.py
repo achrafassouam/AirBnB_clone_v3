ca426d0c8dd17e7fc17435298bcd63e87123c7e2
“Achraf <achrafassouam@gmail.com>
2024-05-28 21:32:14 +0100

    update places.py

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -99,43 +99,43 @@ def update_place(place_id):
     place.save()
     return make_response(jsonify(place.to_dict()), 200)
 
-    @app_views.route('/places_search', methods=['POST'])
-    def places_search():
-        """
-        Retrieves all Place objects based on the JSON body request
-        """
-        if request.get_json() is not None:
-            params = request.get_json()
-            states = params.get('states', [])
-            cities = params.get('cities', [])
-            amenities = params.get('amenities', [])
-            amenity_objects = []
-            for amenity_id in amenities:
-                amenity = storage.get('Amenity', amenity_id)
-                if amenity:
-                    amenity_objects.append(amenity)
-            if states == cities == []:
-                places = storage.all('Place').values()
-            else:
-                places = []
-                for state_id in states:
-                    state = storage.get('State', state_id)
-                    state_cities = state.cities
-                    for city in state_cities:
-                        if city.id not in cities:
-                            cities.append(city.id)
-                for city_id in cities:
-                    city = storage.get('City', city_id)
-                    for place in city.places:
-                        places.append(place)
-            confirmed_places = []
-            for place in places:
-                place_amenities = place.amenities
-                confirmed_places.append(place.to_dict())
-                for amenity in amenity_objects:
-                    if amenity not in place_amenities:
-                        confirmed_places.pop()
-                        break
-            return jsonify(confirmed_places)
+@app_views.route('/places_search', methods=['POST'])
+def places_search():
+    """
+    Retrieves all Place objects based on the JSON body request
+    """
+    if request.get_json() is not None:
+        params = request.get_json()
+        states = params.get('states', [])
+        cities = params.get('cities', [])
+        amenities = params.get('amenities', [])
+        amenity_objects = []
+        for amenity_id in amenities:
+            amenity = storage.get('Amenity', amenity_id)
+            if amenity:
+                amenity_objects.append(amenity)
+        if states == cities == []:
+            places = storage.all('Place').values()
         else:
-            return make_response(jsonify({'error': 'Not a JSON'}), 400)
+            places = []
+            for state_id in states:
+                state = storage.get('State', state_id)
+                state_cities = state.cities
+                for city in state_cities:
+                    if city.id not in cities:
+                        cities.append(city.id)
+            for city_id in cities:
+                city = storage.get('City', city_id)
+                for place in city.places:
+                    places.append(place)
+        confirmed_places = []
+        for place in places:
+            place_amenities = place.amenities
+            confirmed_places.append(place.to_dict())
+            for amenity in amenity_objects:
+                if amenity not in place_amenities:
+                    confirmed_places.pop()
+                    break
+        return jsonify(confirmed_places)
+    else:
+        return make_response(jsonify({'error': 'Not a JSON'}), 400)
